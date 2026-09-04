d60ccd3a17483e167066c39fbbc3fb4aa8c99add
Anurag Mehta <anurag.mehta@insight.com>
2026-09-04 12:51:34 +0530

    Deploy Landing Zone Network

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-landingzone-dev"
+  location = "Central India"
+}
+resource "azurerm_virtual_network" "hub" {
+  name                = "hub-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+resource "azurerm_virtual_network" "spoke" {
+  name                = "spoke-vnet"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
